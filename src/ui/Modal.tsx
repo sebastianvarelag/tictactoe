2b271c2d69f7611dc684bed1ba2c095598fe97c4
Sebastian <sebastian.varelagi@gmail.com>
2024-09-11 10:40:00 -0500

    Modal added.

---
+++ src/ui/Modal.tsx
@@ -0,0 +1,12 @@
+export const Modal = () => {
+  return (
+    <>
+      <div className="modal__container">
+        <div className="modal__content">
+          <h2>GAME OVER</h2>
+          <p>Player <span style={{color: '#FF8A8A'}}>X</span> wins!</p>
+        </div>
+      </div>
+    </>
+  )
+}
