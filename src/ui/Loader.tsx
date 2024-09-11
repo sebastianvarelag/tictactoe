1003b4116ef43d11238150c3c5f157839e6fe3be
Sebastian <sebastian.varelagi@gmail.com>
2024-09-11 13:10:39 -0500

    Add Loader and styles

---
+++ src/ui/Loader.tsx
@@ -0,0 +1,13 @@
+export const Loader = () => {
+	return (
+		<>
+			<div className="loading__container">
+				<div className="loading-spinner-rolling">
+					<div className="loader">
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
